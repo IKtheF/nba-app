471c9a59ad95779f60d16191027bb54a876435ff
IKtheF <90979460+IKtheF@users.noreply.github.com>
2021-11-04 02:08:18 +0900

    3rd commit

--- Chart.py
+++ Chart.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def app():
     st.title('～数字で見るNBA～')
