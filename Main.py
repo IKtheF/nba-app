279d1b2a9b59f4ffc326a320e6ce53016b8cc715
IKtheF <90979460+IKtheF@users.noreply.github.com>
2021-11-04 01:48:37 +0900

    initial commit
    This is the first commit.

---
+++ Main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from Transcribe import *
+from googletrans import Translator
+tr = Translator()
+
+def app():
+    
+    st.title("NBA × 英語で学ぶ")
+    st.header('～音声ファイルの文字起こし ＆ 翻訳～')
+    fileObject = st.file_uploader(label = "音声ファイルをアップロードしてください" )
+
+    if fileObject:
+        token, t_id = upload_file(fileObject)
+        result = {}
+
+        with st.spinner("少々お待ちください....."):
+            while result.get("status") != 'completed':
+                result = get_text(token,t_id)
+
+        trans=tr.translate(result['text'],src='en',dest='ja').text
+
+        st.balloons()
+        st.subheader("▼文字起こしの結果")
+        st.write(result['text'])
+
+        st.subheader('▼翻訳結果')
+        st.write(trans)
+    
+    st.write('--------------------------------------------------')
+
+    st.header('～お好きな言葉を翻訳～')
+    input_text=st.text_input('翻訳したい言葉を入力して下さい')
+
+    st.write('▼言語選択')
+    pre_option=st.selectbox('翻訳前の言語を選択してください（日本語を英訳するなら「日本語」を選択）',
+    ('日本語','英語'))
+    st.write('選択中の言語:',pre_option)
+
+    if pre_option == '英語':
+        pre='en'
+        post='ja'
+    else:
+        pre='ja'
+        post='en'
+
+    if input_text is not None:
+        if st.button('翻訳開始！'):
+            result = tr.translate(input_text,src=pre,dest=post).text
+            st.subheader('▼翻訳結果')
+            st.write(result)
---
+++ Title.py
@@ -0,0 +1,19 @@
+import Home
+import Chart
+import Quize
+import Use
+import Main
+import streamlit as st
+
+PAGES={
+    "ホーム":Home,
+    "音声ファイルの文字起こし＆翻訳":Main,
+    "数字で見るNBA": Chart,
+    "NBAクイズ":Quize,
+    "使い方": Use
+}
+
+st.sidebar.title('MENU')
+selection=st.sidebar.radio("選択してください", list(PAGES.keys()))
+page=PAGES[selection]
+page.app()
