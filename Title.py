1f7c7a750c2f4a6b9d08c83cb7710e5281edd0ce
IKtheF <90979460+IKtheF@users.noreply.github.com>
2021-11-04 19:27:04 +0900

    Update Title.py

--- Title.py
+++ Title.py
@@ -1,5 +1,5 @@
 import Home
-import Chart
+#import Chart
 import Quize
 import Use
 import Main
@@ -8,7 +8,7 @@ import streamlit as st
 PAGES={
     "ホーム":Home,
     "音声ファイルの文字起こし＆翻訳":Main,
-    "数字で見るNBA": Chart,
+    #"数字で見るNBA": Chart,
     "NBAクイズ":Quize,
     "使い方": Use
 }
