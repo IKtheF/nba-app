19bc835c7761793b912c7ec9416489933293a4cd
IKtheF <90979460+IKtheF@users.noreply.github.com>
2021-11-05 22:50:24 +0900

    Update Chart.py

--- Chart.py
+++ Chart.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import matplotlib.pyplot as plt
+# from plotly.subplots import make_subplots
+# import matplotlib.pyplot as plt
 
 def app():
     st.title('～数字で見るNBA～')
