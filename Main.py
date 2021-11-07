8350c94b240873a40e637a84f71b6772afc23a30
IKtheF <90979460+IKtheF@users.noreply.github.com>
2021-11-07 19:25:57 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -31,25 +31,26 @@ def app():
         st.subheader('▼翻訳結果')
         st.write(trans)
     
-    st.write('--------------------------------------------------')
+#     st.write('--------------------------------------------------')
 
-    st.header('～お好きな言葉を翻訳～')
-    input_text=st.text_input('翻訳したい言葉を入力して下さい')
+#     st.header('～お好きな言葉を翻訳～')
+#     input_text=st.text_input('翻訳したい言葉を入力して下さい')
 
-    st.write('▼言語選択')
-    pre_option=st.selectbox('翻訳前の言語を選択してください（日本語を英訳するなら「日本語」を選択）',
-    ('日本語','英語'))
-    st.write('選択中の言語:',pre_option)
+#     st.write('▼言語選択')
+#     pre_option=st.selectbox('翻訳前の言語を選択してください（日本語を英訳するなら「日本語」を選択）',
+#     ('日本語','英語'))
+#     st.write('選択中の言語:',pre_option)
 
-    if pre_option == '英語':
-        pre='en'
-        post='ja'
-    else:
-        pre='ja'
-        post='en'
+#     if pre_option == '英語':
+#         pre='en'
+#         post='ja'
+#     else:
+#         pre='ja'
+#         post='en'
 
-    if input_text is not None:
-        if st.button('翻訳開始！'):
-            result = tr.translate(input_text,src=pre,dest=post).text
-            st.subheader('▼翻訳結果')
-            st.write(result)
+#     if input_text is not None:
+#         if st.button('翻訳開始！'):
+#             result = tr.translate(input_text,src=pre,dest=post).text
+#             st.subheader('▼翻訳結果')
+#             st.write(result)
+#     st.write('※機種によってはアプリを開いた時にぺージの一番下から始まってしまうことがあります。')
