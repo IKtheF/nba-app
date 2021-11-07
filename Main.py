f14be304b2c479a334dd8c5590f615fce58d47f9
IKtheF <90979460+IKtheF@users.noreply.github.com>
2021-11-07 19:32:11 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,9 +7,6 @@ def app():
     
     st.title("NBA × 英語で学ぶ")
     st.write('▼このページで出来ることは以下の通りです。')
-#     st.write('①パソコンの音声ファイルや、スマホで録画した動画の文字起こし＆翻訳')
-#     st.write('②入力したテキストの和訳または英訳')
-#     st.write('--------------------------------------------------')
     st.write('パソコンの音声ファイルや、スマホで録画した動画の文字起こし＆翻訳')
     
     st.header('～文字起こし ＆ 翻訳～')
@@ -32,26 +29,3 @@ def app():
         st.subheader('▼翻訳結果')
         st.write(trans)
     
-#     st.write('--------------------------------------------------')
-
-#     st.header('～お好きな言葉を翻訳～')
-#     input_text=st.text_input('翻訳したい言葉を入力して下さい')
-
-#     st.write('▼言語選択')
-#     pre_option=st.selectbox('翻訳前の言語を選択してください（日本語を英訳するなら「日本語」を選択）',
-#     ('日本語','英語'))
-#     st.write('選択中の言語:',pre_option)
-
-#     if pre_option == '英語':
-#         pre='en'
-#         post='ja'
-#     else:
-#         pre='ja'
-#         post='en'
-
-#     if input_text is not None:
-#         if st.button('翻訳開始！'):
-#             result = tr.translate(input_text,src=pre,dest=post).text
-#             st.subheader('▼翻訳結果')
-#             st.write(result)
-#     st.write('※機種によってはアプリを開いた時にぺージの一番下から始まってしまうことがあります。')
