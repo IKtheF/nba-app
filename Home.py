c83a2ca9c919b87d7f03355f484068cf39d12fa4
IKtheF <90979460+IKtheF@users.noreply.github.com>
2021-11-07 19:34:25 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,23 +1,24 @@
 import streamlit as st
-import datetime as dt
 
-def app():
-    st.title('This Is My House')
-    now = dt.date.today()
-    st.write(f"Today is {now}")
-    st.image('nba_all_team.jpg')
+def app():    
+    st.header('～お好きな言葉を翻訳～')
+    input_text=st.text_input('翻訳したい言葉を入力して下さい')
 
-    st.header('概要')
-    st.write('このアプリは、TwitterなどでNBA選手たちのインタビューを見ていて「この人たちが何を言っているのか知りたい！」、「選手たちの生きた英語を利用して英語の学習がしたい！」\
-    と思い作成したものです。')
-    st.write('メインの機能は「音声ファイルの文字起こし＆翻訳」ですが、その他にも様々な機能を搭載しているので是非使用してみて下さい。詳しい使い方は、左のサイドバーの「使い方」からご覧ください。')
+    st.write('▼言語選択')
+    pre_option=st.selectbox('翻訳前の言語を選択してください（日本語を英訳するなら「日本語」を選択）',
+    ('日本語','英語'))
+    st.write('選択中の言語:',pre_option)
 
-    st.header('お知らせ')
-    st.write('また、InstagramにNBAの絵を投稿しています。もし興味を持っていただけたなら、一度覗いてみてください！フォローもしていただけると嬉しいです！！')
-    st.write('リンクに飛べない方は"l3gacy_flash"で検索していただけると幸いです。')
-    st.markdown('<a href="https://www.instagram.com/l3gacy_flash">Instagramはこちらから！</a>',unsafe_allow_html=True)
+    if pre_option == '英語':
+        pre='en'
+        post='ja'
+    else:
+        pre='ja'
+        post='en'
 
-    st.header('おまけ')
-    music='videoplayback.mp4'
-    st.write('NBA好きならテンション上がること間違いなしの音楽を貼っておくので、ぜひ聞いてみてください！')
-    st.audio(music)
+    if input_text is not None:
+        if st.button('翻訳開始！'):
+            result = tr.translate(input_text,src=pre,dest=post).text
+            st.subheader('▼翻訳結果')
+            st.write(result)
+    st.write('※機種によってはアプリを開いた時にぺージの一番下から始まってしまうことがあります。')
